80d3c10d8f2e01b9cd24657ef370d3e46ab18861
Srijan Shovit <srijanshovit@gmail.com>
2021-11-17 12:21:05 +0530

    deleted unwanted files

--- lib/CustomClasses/my_controller2.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:learning_get/CustomClasses/student.dart';
-
-class MyController2 extends GetxController {
-  var count = 0.obs;
-  void increment() {
-    count++;
-  }
-}
