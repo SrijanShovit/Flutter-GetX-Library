faef238d171637066e643d20a16a1e22e609cab7
Srijan Shovit <srijanshovit@gmail.com>
2021-11-17 12:37:27 +0530

    id

--- lib/CustomClasses/my_controller4.dart
+++ lib/CustomClasses/my_controller4.dart
@@ -3,25 +3,8 @@ import 'package:learning_get/CustomClasses/student.dart';
 
 class MyController4 extends GetxController {
   var count = 0;
-  void increment() async {
-    await Future<int>.delayed(Duration(seconds: 5));
-    this.count++;
-    update();
-  }
-
-  void cleanUpTask() {
-    print("Clean up task");
-  }
-
-  //Better approach
-  @override
-  void onInit() {
-    print("Init called");
-    super.onInit();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
+  void increment() {
+    count++;
+    update(['txtCount']); //only update getbuilder with this id
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,45 +7,40 @@ void main() {
   runApp(MyApp());
 }
 
-// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  // ignore: non_constant_identifier_names
-  MyController4 my_controller = Get.put(MyController4());
+  MyController4 myController = Get.put(MyController4());
 
   @override
   Widget build(BuildContext context) {
     //if only state management is required then MaterialApp will be do good
     //for additional purposes of get we need GetMaterialApp(without using context)
     return GetMaterialApp(
-        title: 'state management',
+        title: 'Unique ID',
         home: Scaffold(
           appBar: AppBar(
-            title: Text('simple state manager'),
+            title: Text('Unique ID'),
           ),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 GetBuilder<MyController4>(
-                  //taking some action while initialising
-                  // initState: (data) => my_controller.increment(),
-
-                  //if we want to take some action while disposing
-                  // dispose: (_) => my_controller.cleanUpTask(),
+                  id: 'txtCount',
                   builder: (controller) {
                     return Text("Count is ${controller.count}");
                   },
                 ),
-                // SizedBox(height: 20),
-                // ElevatedButton(
-                //     onPressed: () {
-                //       //if instance created at top
-                //       my_controller.increment();
-
-                //       //if instance not created at top
-                //       // Get.find<MyController3>().increment();
-                //     },
-                //     child: Text('Increment'))
+                GetBuilder<MyController4>(
+                  builder: (controller) {
+                    return Text("Count is ${controller.count}");
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () {
+                      myController.increment();
+                    },
+                    child: Text('Increment'))
               ],
             ),
           ),
