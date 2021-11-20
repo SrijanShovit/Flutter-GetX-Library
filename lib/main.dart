df69fb4f9186bcd20cd7b425c475f26e949fbe00
Srijan Shovit <srijanshovit@gmail.com>
2021-11-20 14:29:02 +0530

    incomplete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:learning_get/CustomClasses/messages.dart';
 
 import 'package:learning_get/CustomClasses/my_controller4.dart';
 
@@ -11,37 +10,35 @@ void main() {
 //Workers generate some specific callbacks whenever some task is done
 
 class MyApp extends StatelessWidget {
-  MyController4 myController = Get.put(MyController4());
+  // MyController4 myController = Get.put(MyController4(),
+  // tag: 'instance1',
+  // permanent: true);
+
+//  Get.lazyPut<MyController4>(
+//     () => MyController4(),
+//   );
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        translations: Messages(),
-        locale: Locale('en',
-            'US'), //default locale //to get device locale : Get.deviceLocale
-        fallbackLocale: Locale('en', 'US'),
-        title: 'Internationalisation',
+        title: 'Dependency Injection',
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Internationalisation'),
+            title: Text('Dependency Injection'),
           ),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('hello'.tr),
                 SizedBox(height: 20),
                 ElevatedButton(
-                    child: Text('Hindi'),
-                    onPressed: () => myController.changeLanguage('hi', 'IN')),
-                SizedBox(height: 20),
-                ElevatedButton(
-                    child: Text('French'),
-                    onPressed: () => myController.changeLanguage('fr', 'FR')),
-                SizedBox(height: 20),
-                ElevatedButton(
-                    child: Text('English'),
-                    onPressed: () => myController.changeLanguage('en', 'US')),
+                    child: Text('Click me'),
+                    onPressed: () {
+                      //If instance is created using tag,we can find instance using tag
+                      // Get.find<MyController4>(tag: 'instance1');
+
+                      Get.find<MyController4>();
+                    }),
               ],
             ),
           ),
